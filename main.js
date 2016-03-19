e1d018450d0d390727d67e226f421cf90e14fa3b
Mohammed Zama Khan <mohammedzamakhan@gmail.com>
2016-03-19 09:02:05 -0700

    Including main.js functioanlity

---
+++ main.js
@@ -0,0 +1,28 @@
+//Variables
+
+/* 
+  This program is an example for class
+  
+  This is multi line comment
+*/
+
+var name = 'Abdullah'; // type string
+// Declaration of Variables
+
+
+var age = 26; // type number 
+
+var isMarried = false; // type Booleans 
+
+var isTeaching = undefined; //type undefined
+//isTeaching = undefined
+
+name = undefined;
+isTeaching = null; //type null
+
+name = 26;
+
+
+
+console.log(typeof name, typeof age, typeof isMarried, typeof isTeaching);
+//Using the value
